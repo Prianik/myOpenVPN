94a8c732e3944ade3a36ca2093a1843111e0637e
Prianik <prianikov@gmail.com>
2025-02-27 21:42:04 +0300

    Update down.sh

--- cmd/down.sh
+++ cmd/down.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-
-#chmod 777 /var/log/openvpn/client-connect.log
-
 echo -e "$(date +%Y-%m-%d) $(date +%H:%M:%S) $common_name $trusted_ip <--- $ifconfig_pool_remote_ip DOWN $script_type $bytes_received $bytes_send"   >> /var/log/openvpn/client-connect.log
 exit 0
