7e4b4e636de7577be103caefb5a5583943388d28
Prianik <prianikov@gmail.com>
2025-02-27 21:43:44 +0300

    Update vpn-stat.sh

--- cmd/vpn-stat.sh
+++ cmd/vpn-stat.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-#echo "Date Time Name IP1 --  IP2  TYPE TYPE2 RECIVE SEND" | awk '{print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10}' | column -c 5 -t
 cat /var/log/openvpn/client-connect.log | awk '{print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10}'| grep "$1" | column -c 5 -t
