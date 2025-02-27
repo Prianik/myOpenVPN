7fc44407f4aa9b2fc29bc238aeeff2b04c809277
Prianik <prianikov@gmail.com>
2025-02-27 21:41:18 +0300

    Update revokeClient.sh

--- cmd/install/revokeClient.sh
+++ cmd/install/revokeClient.sh
@@ -12,49 +12,45 @@ CCD=$(cat ${ServerDir}/server.conf  | grep client-config-dir | awk '{print $2}')
 PASS=1
 CLIENT=$1
 
-
-
-
-
-	NUMBEROFCLIENTS=$(tail -n +2 /etc/openvpn/easy-rsa/pki/index.txt | grep -c "^V")
-	if [[ $NUMBEROFCLIENTS == '0' ]]; then
-		echo ""
-		echo "You have no existing clients!"
-		exit 1
-	fi
-
+NUMBEROFCLIENTS=$(tail -n +2 /etc/openvpn/easy-rsa/pki/index.txt | grep -c "^V")
+if [[ $NUMBEROFCLIENTS == '0' ]]; then
 	echo ""
-	echo "Select the existing client certificate you want to revoke"
-	tail -n +2 /etc/openvpn/easy-rsa/pki/index.txt | grep "^V" | cut -d '=' -f 2 | nl -s ') '
-	until [[ $CLIENTNUMBER -ge 1 && $CLIENTNUMBER -le $NUMBEROFCLIENTS ]]; do
-		if [[ $CLIENTNUMBER == '1' ]]; then
-			read -rp "Select one client [1]: " CLIENTNUMBER
-		else
-			read -rp "Select one client [1-$NUMBEROFCLIENTS]: " CLIENTNUMBER
-		fi
-	done
-	CLIENT=$(tail -n +2 /etc/openvpn/easy-rsa/pki/index.txt | grep "^V" | cut -d '=' -f 2 | sed -n "$CLIENTNUMBER"p)
-	cd /etc/openvpn/easy-rsa/ || return
-	./easyrsa --batch revoke "$CLIENT"
-	EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl
-	rm -f /etc/openvpn/crl.pem
-	cp /etc/openvpn/easy-rsa/pki/crl.pem /etc/openvpn/crl.pem
-	chmod 644 /etc/openvpn/crl.pem
-	find /home/ -maxdepth 2 -name "$CLIENT.ovpn" -delete
-	rm -f "/root/$CLIENT.ovpn"
-	sed -i "/^$CLIENT,.*/d" /etc/openvpn/ipp.txt
-	cp /etc/openvpn/easy-rsa/pki/index.txt{,.bk}
-#-----
+	echo "You have no existing clients!"
+	exit 1
+fi
+
+echo ""
+echo "Select the existing client certificate you want to revoke"
+tail -n +2 /etc/openvpn/easy-rsa/pki/index.txt | grep "^V" | cut -d '=' -f 2 | nl -s ') '
+until [[ $CLIENTNUMBER -ge 1 && $CLIENTNUMBER -le $NUMBEROFCLIENTS ]]; do
+	if [[ $CLIENTNUMBER == '1' ]]; then
+		read -rp "Select one client [1]: " CLIENTNUMBER
+	else
+		read -rp "Select one client [1-$NUMBEROFCLIENTS]: " CLIENTNUMBER
+	fi
+done
+CLIENT=$(tail -n +2 /etc/openvpn/easy-rsa/pki/index.txt | grep "^V" | cut -d '=' -f 2 | sed -n "$CLIENTNUMBER"p)
+cd /etc/openvpn/easy-rsa/ || return
+./easyrsa --batch revoke "$CLIENT"
+EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl
+rm -f /etc/openvpn/crl.pem
+cp /etc/openvpn/easy-rsa/pki/crl.pem /etc/openvpn/crl.pem
+chmod 644 /etc/openvpn/crl.pem
+
+find /home/ -maxdepth 2 -name "$CLIENT.ovpn" -delete
+rm -f "/root/$CLIENT.ovpn"
+sed -i "/^$CLIENT,.*/d" /etc/openvpn/ipp.txt
+cp /etc/openvpn/easy-rsa/pki/index.txt{,.bk}
+
 rm -f $homeDir/$CLIENT.ovpn 
 rm -f $CCD/$CLIENT
 sed -i "/^$CLIENT*/d" "$USER_VPN_FILE"
+
 # удаляю пустые строки
 sed -i '/^$/d' "$USER_VPN_FILE"
 
 # Сортировка файла user-vpn.txt по возрастанию
 sort -o "$USER_VPN_FILE" "$USER_VPN_FILE"
-#-----
-	echo ""
-	echo "Certificate for client $CLIENT revoked."
 
-
+echo ""
+echo "Certificate for client $CLIENT revoked."
